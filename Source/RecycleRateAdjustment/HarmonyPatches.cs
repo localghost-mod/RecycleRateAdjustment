6e845b541e5461907c96337bfbba5d7fe600925c
zhikangluo <155446689+localghost-mod@users.noreply.github.com>
2024-01-22 11:07:48 +0800

    Init

---
+++ Source/RecycleRateAdjustment/HarmonyPatches.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Verse;
+
+namespace RecycleRate
+{
+    [StaticConstructorOnStartup]
+    public static class Startup
+    {
+        static Startup() => new Harmony("localghost.RecycleRateAdjustment").PatchAll();
+    }
+
+    [HarmonyPatch(typeof(Thing), nameof(Thing.SmeltProducts), MethodType.Enumerator)]
+    public static class SmeltProductsPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldc_R4)
+                {
+                    yield return new CodeInstruction(
+                        OpCodes.Ldsfld,
+                        typeof(RecycleRateMod).GetField(nameof(RecycleRateMod.settings))
+                    );
+                    yield return new CodeInstruction(
+                        OpCodes.Ldfld,
+                        typeof(Settings).GetField(nameof(Settings.recycleRate))
+                    );
+                }
+                else
+                    yield return instruction;
+            }
+        }
+    }
+}
---
+++ Source/RecycleRateAdjustment/Settings.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace RecycleRate
+{
+    public class Settings : ModSettings
+    {
+        public float recycleRate = 0.25f;
+
+        public override void ExposeData() => Scribe_Values.Look(ref recycleRate, "recycleRate");
+    }
+
+    public class RecycleRateMod : Mod
+    {
+        public static Settings settings;
+
+        public RecycleRateMod(ModContentPack content)
+            : base(content)
+        {
+            settings = GetSettings<Settings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard ls = new Listing_Standard();
+            ls.Begin(inRect);
+            ls.SliderLabeled(
+                "RR.RecycleRate".Translate(),
+                ref settings.recycleRate,
+                settings.recycleRate.ToStringPercent(),
+                0.01f,
+                1.00f
+            );
+            if (ls.ButtonText("RR.Reset".Translate()))
+                settings.recycleRate = 0.25f;
+            ls.End();
+        }
+
+        public override string SettingsCategory() => "RR.RecycleRateAdjustment".Translate();
+    }
+
+    public static class Listing_StandardExtension
+    {
+        public static void SliderLabeled(
+            this Listing_Standard ls,
+            string label,
+            ref float val,
+            string format,
+            float min = 0f,
+            float max = 1f,
+            string tooltip = null
+        )
+        {
+            Rect rect = ls.GetRect(Text.LineHeight);
+            Rect rect2 = GenUI.Rounded(GenUI.LeftPart(rect, 0.5f));
+            Rect rect3 = GenUI.Rounded(
+                GenUI.LeftPart(GenUI.Rounded(GenUI.RightPart(rect, 0.3f)), 0.67f)
+            );
+            Rect rect4 = GenUI.Rounded(GenUI.RightPart(rect, 0.1f));
+            TextAnchor anchor = Text.Anchor;
+            Text.Anchor = TextAnchor.MiddleLeft;
+            Widgets.Label(rect2, label);
+            float num = Widgets.HorizontalSlider_NewTemp(rect3, val, min, max);
+            val = num;
+            Text.Anchor = TextAnchor.MiddleRight;
+            Widgets.Label(rect4, string.Format(format, val));
+            if (!GenText.NullOrEmpty(tooltip))
+            {
+                TooltipHandler.TipRegion(rect, tooltip);
+            }
+            Text.Anchor = anchor;
+            ls.Gap(ls.verticalSpacing);
+        }
+    }
+}
